5d513b5877841ac60511ff1a5677eb68c2863699
Justin George <adhijustin007@gmail.com>
2024-03-29 21:34:21 +0100

    Code adjustments for the release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.justin.clippedcircleshapesample"
         minSdk = 28
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
